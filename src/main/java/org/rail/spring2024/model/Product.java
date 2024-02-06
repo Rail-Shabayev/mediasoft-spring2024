8f67ef687037c5cb098f8b660d4b608e501bc166
rail <rail.shabayev@mail.ru>
2024-02-06 19:33:13 +0400

    feat: add Product class

---
+++ src/main/java/org/rail/spring2024/model/Product.java
@@ -0,0 +1,30 @@
+package org.rail.spring2024.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+    private String name;
+    private String desc;
+    private ProductType type;
+    private BigDecimal price;
+    private int quantity;
+    private LocalDateTime dateUpdated;
+    private LocalDateTime dateCreated;
+}
