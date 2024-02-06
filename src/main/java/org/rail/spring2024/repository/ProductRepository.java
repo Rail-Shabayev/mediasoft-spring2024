3b57779a60f13608a518be213a996e1c82033b47
rail <rail.shabayev@mail.ru>
2024-02-06 19:36:48 +0400

    feat: add ProductRepository class

---
+++ src/main/java/org/rail/spring2024/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.rail.spring2024.repository;
+
+import org.rail.spring2024.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
