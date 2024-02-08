b89bd1de2dc79d0ce659c11d02a5364c2538f2ff
rail <rail.shabayev@mail.ru>
2024-02-08 10:47:36 +0400

    test: add tests for saveProduct and mapToEntity methods

--- src/test/java/org/rail/spring2024/service/ProductServiceTest.java
+++ src/test/java/org/rail/spring2024/service/ProductServiceTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Captor;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.rail.spring2024.dto.ProductDTO;
 import org.rail.spring2024.model.Product;
@@ -15,7 +17,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.rail.spring2024.model.ProductType.ELECTRONICS;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +32,9 @@ public class ProductServiceTest {
 
     @Mock
     ProductRepository productRepository;
+
+    @Captor
+    private ArgumentCaptor<Product> productArgumentCaptor;
 
     Product product1 = new Product
             (null, "laptop", "gaming laptop", ELECTRONICS,
@@ -58,5 +63,23 @@ public class ProductServiceTest {
         List<ProductDTO> actualProductList = productService.getAllProducts();
 
         assertThat(actualProductList.get(0).getName()).isEqualTo(productDTOList.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("should map Dto to the Product Entity")
+    void shouldMapDtoToEntity() {
+        Product product = productService.mapToEntity(productDto1);
+        assertThat(product.getName()).isEqualTo(productDto1.getName());
+        assertThat(product.getPrice()).isNotEqualTo(productDto1.getQuantity());
+    }
+
+    @Test
+    @DisplayName("should save ProductDto")
+    void shouldSaveProductDto() {
+        productRepository.save(product1);
+        verify(productRepository, times(1)).save(productArgumentCaptor.capture());
+
+        assertThat(productArgumentCaptor.getValue().getName()).isEqualTo("laptop");
+        assertThat(productArgumentCaptor.getValue().getDescription()).isEqualTo("gaming laptop");
     }
 }
