c12732ce7b09dbafe9bfe089add2ff6c840b00bd
rail <rail.shabayev@mail.ru>
2024-02-08 21:02:05 +0400

    feat: add DataLoader class
    preload Data to the database to be able work with PUT and GET methods

---
+++ src/main/java/org/rail/spring2024/config/DataLoader.java
@@ -0,0 +1,35 @@
+package org.rail.spring2024.config;
+
+import org.rail.spring2024.model.Product;
+import org.rail.spring2024.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static java.util.UUID.randomUUID;
+import static org.rail.spring2024.model.ProductType.*;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner runner(ProductRepository productRepository) {
+        return args -> {
+            Product product1 = new Product
+                    (randomUUID(), "laptop", "gaming laptop", ELECTRONICS,
+                            new BigDecimal("2"), 2, null, LocalDate.now());
+            Product product2 = new Product
+                    (randomUUID(), "snikers", "best snikers", FOOTWEAR,
+                            new BigDecimal("1"), 5, null, LocalDate.now());
+            Product product3 = new Product
+                    (randomUUID(), "purse", "gucci purse", ACCESSORIES,
+                            new BigDecimal("3"), 10, null, LocalDate.now());
+            productRepository.save(product1);
+            productRepository.save(product2);
+            productRepository.save(product3);
+        };
+    }
+}
