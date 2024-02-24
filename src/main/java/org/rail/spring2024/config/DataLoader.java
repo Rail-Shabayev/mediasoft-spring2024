92a7fc5b9908cbb03a109be8163dcd7c75c89a46
rail <rail.shabayev@mail.ru>
2024-02-24 06:59:45 +0400

    fix: change dateQuantityUpdated property in Product objects
    prevent postman test from failing

--- src/main/java/org/rail/spring2024/config/DataLoader.java
+++ src/main/java/org/rail/spring2024/config/DataLoader.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static java.util.UUID.randomUUID;
 import static org.rail.spring2024.model.ProductType.*;
@@ -20,13 +21,13 @@ public class DataLoader {
         return args -> {
             Product product1 = new Product
                     (randomUUID(), "laptop", "gaming laptop", ELECTRONICS,
-                            new BigDecimal("2"), 2, null, LocalDate.now());
+                            new BigDecimal("2"), 2, LocalDateTime.now(), LocalDate.now());
             Product product2 = new Product
                     (randomUUID(), "snikers", "best snikers", FOOTWEAR,
-                            new BigDecimal("1"), 5, null, LocalDate.now());
+                            new BigDecimal("1"), 5, LocalDateTime.now(), LocalDate.now());
             Product product3 = new Product
                     (randomUUID(), "purse", "gucci purse", ACCESSORIES,
-                            new BigDecimal("3"), 10, null, LocalDate.now());
+                            new BigDecimal("3"), 10, LocalDateTime.now(), LocalDate.now());
             productRepository.save(product1);
             productRepository.save(product2);
             productRepository.save(product3);
