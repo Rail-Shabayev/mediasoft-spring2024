67be1d8e0059e6baf74bd44eaf840a7a2937bea7
rail <rail.shabayev@mail.ru>
2024-03-19 09:15:06 +0400

    fix: change default server from localhost:8080/api/product to localhost:8080
    when making a request through openApi. It was calling to an endpoint of
     localhost:8080/api/product/api/product

--- src/main/java/org/rail/spring2024/config/OpenApiConfig.java
+++ src/main/java/org/rail/spring2024/config/OpenApiConfig.java
@@ -22,7 +22,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                         description = "Local environment",
-                        url = "http://localhost:8080/api/product"
+                        url = "http://localhost:8080"
                 )
         }
 )
